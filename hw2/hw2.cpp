d24f692ac1cfe4f936f364694c3987e769f7b3fe
Seasa_Huang <ericet1234@gmail.com>
2018-03-27 21:32:19 +0800

    simple input and parser finish

---
+++ hw2/hw2.cpp
@@ -0,0 +1,75 @@
+#include<vector>
+#include<stdio.h>
+#include<sstream>
+#include<iostream>
+using namespace std;
+
+class fp_growth{
+    private:
+        double freq;
+        string input;
+        string output
+        vector< vector<int> >  transactions();
+
+    public:
+        fp_growth(double freq,string input,string output)
+        {
+            this.freq = freq;
+            this.input = input;
+            this.output = output;
+        }
+        void input()
+        {
+            freopen(this.input.c_str(),"r", stdin);
+            string line;
+            //read until EOF
+            
+            while(!getline(cin, line).eof())
+            {
+                vector<int> arr;
+                istringstream ssline(line);
+                
+                string number;
+                while(getline(ssline, number, ','))
+                    arr.push_back(atoi(number.c_str()));
+                
+                transactions.push_back(arr);
+            }
+                
+            /*
+            for(int i=0 ; i < transactions.size() ; i++)
+            {
+                for(int j=0 ; j < transactions[i].size() ; j++)
+                    printf("%d ",transactions[i][j]);
+                printf("\n");
+            }
+            */    
+        }
+        
+}
+
+
+
+
+int main(int argc,char *argv[])
+{
+    if(argc != 4)
+    {
+        fprintf(stderr,"there should be 4 command line input\n");
+        return 0;
+    }
+    double freq;
+    if(sscanf(argv[1],"%lf",&freq) == 0)
+    {
+        fprintf(stderr,"the first input should be number\n");
+        return 0;    
+    }
+
+    fp_growth test(freq,argv[2],argv[3])
+
+    fp_build();
+    fp_mining();
+    fp_output();
+    
+    return 0;
+}
